b698fbe8fdbf7f737552a1b2ad15e97b74b91a5f
Lucia Bide <luciabide9@gmail.com>
2024-10-29 20:12:45 -0300

    si

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,38 @@
 import { user } from "../utils/loginData.js";
+const form = document.querySelector("#form");
+const errorText = document.querySelector("#error");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  console.log("Email: ", email);
+  console.log("Password: ", password);
+
+  if (user.email === email && user.password === password) {
+    localStorage.setItem("userEmail", email);
+    window.location.href = "./index.html";
+  } else {
+    errorText.style.display = "flex";
+    form.elements.email.value = "";
+    form.elements.password.value = "";
+  }
+});
+
+const userEmail = localStorage.getItem("userEmail");
+
+if (userEmail) {
+  window.location.href = "./index.html";
+}
+
+console.log(user);
 
 /*const form = document.querySelector("#form");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    event.preventDefault(); https://www.youtube.com/watch?v=OsRLKcclsRQ&ab_channel=armxndo
 
         const email = form.elements.email.value;
         const password = form.elements.password.value;
