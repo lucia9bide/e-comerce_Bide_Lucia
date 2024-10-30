ac47417ff9b5cbcab019b8f5bfe3f54ce42876fc
Lucia Bide <luciabide9@gmail.com>
2024-10-30 12:26:34 -0300

    haciendo el log

--- pages/login.js
+++ pages/login.js
@@ -1,57 +1,27 @@
 import { user } from "../utils/loginData.js";
-const form = document.querySelector("#form");
-const errorText = document.querySelector("#error");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
 
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
 
-  console.log("Email: ", email);
-  console.log("Password: ", password);
+    console.log("Email: ", email);
+    console.log("Password: ", password);
 
-  if (user.email === email && user.password === password) {
-    localStorage.setItem("userEmail", email);
-    window.location.href = "./index.html";
-  } else {
-    errorText.style.display = "flex";
-    form.elements.email.value = "";
-    form.elements.password.value = "";
-  }
+    if (user.email === email && user.password === password){
+        let quantity = 0;
+
+        localStorage.setItem("emailUser", email);
+
+        localStorage.setItem("cart", JSON.stringify([]));
+        localStorage.setItem("quantity", quantity);
+
+        window.location.href = "../index.html";
+    }else{
+        form.elements.email.value = "";
+        form.elements.password.value = "";
+    }
 });
 
-const userEmail = localStorage.getItem("userEmail");
-
-if (userEmail) {
-  window.location.href = "./index.html";
-}
-
-console.log(user);
-
-/*const form = document.querySelector("#form");
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault(); https://www.youtube.com/watch?v=OsRLKcclsRQ&ab_channel=armxndo
-
-        const email = form.elements.email.value;
-        const password = form.elements.password.value;
-
-        console.log("Email: ", email);
-        console.log("Contraseña: ", password);
-
-        if(userData.email === email && userData.password === password){
-            localStorage.setItem("email", user);
-            window.location.href = "./index.html";
-        }else{
-            alert("Email o contraseña incorrectos");
-        };
-});
-
-const userEmail = localStorage.getItem("userEmail");
-
-if (userEmail) {
-  window.location.href = "./index.html";
-}
-
-console.log(user);*/
+/*https://www.youtube.com/watch?v=OsRLKcclsRQ&ab_channel=armxndo*/
